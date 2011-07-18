8d762f7bdf64d389b37a4965023e69b26187a293
unknown <Lidka@.(none)>
2011-07-18 09:19:16 +0300

    bbb

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
   function db_connect()
   {
-	  $resource = mysql_connect('localhost','root','lida900128') or die("Could not connect".mysql_error());
+	  $resource = mysql_connect('localhost','root','') or die("Could not connect".mysql_error());
 	  mysql_query('set names utf8',$resource);
 	  mysql_select_db('photoalbum',$resource)or die("Could not find database".mysql_error());
 	  return $resource;
